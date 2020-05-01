6eff30b815da4bc14ade8bcbbed365090360b123
Bhamoss <26162072+Bhamoss@users.noreply.github.com>
2020-05-02 00:27:52 +0200

    notifications & articles

--- mylib.js
+++ mylib.js
@@ -1,13 +1,11 @@
 function home() {
-  document.querySelector("#content").innerHTML = "home";
-
-  var soql = "SELECT Text__c FROM Knowledge__kav LIMIT 100";
+  var soql = "SELECT Title, Summary, UrlName FROM Knowledge__kav LIMIT 100";
   force.query(
     soql,
     function (data) {
       var records = data.records;
 
-      var start = '<ul id="contacts" class="table-view content">';
+      var start = '<ul class="table-view">';
       var end = "</ul>";
       var mid = "";
 
@@ -20,18 +18,54 @@ function home() {
       });
       */
       for (var i = 0; i < records.length; i++) {
-        if (records[i].Text__c != null) {
-          mid +=
-            '<li class="table-view-cell"><div class="media-body">' +
-            records[i].Text__c +
-            "</div></li>";
+        mid +=
+          '<li class="table-view-cell media"><a class="navigate-right" onclick="article(' +
+          "'" +
+          records[i].UrlName +
+          "'" +
+          ')"><div class="media-body">' +
+          records[i].Title +
+          "<p>";
+        if (records[i].Summary != null) {
+          mid += records[i].Summary;
         }
+        mid += "</p></div></a></li>";
       }
 
       document.querySelector("#content").innerHTML = start + mid + end;
     },
     function (error) {
       alert("Failed to fetch contacts: " + error);
+    }
+  );
+}
+
+function article(url) {
+  var soql =
+    "SELECT Title, Summary, Text__c FROM Knowledge__kav k WHERE k.UrlName = '" +
+    url +
+    "'";
+  force.query(
+    soql,
+    function (data) {
+      var records = data.records;
+
+      var start = '<div class="content-padded">';
+      var end = "</div>";
+      var mid = "";
+
+      mid += "<h2>" + records[0].Title + "</h2>";
+      if (records[0].Summary != null) {
+        mid += "<h4>" + records[0].Summary + "</h4>";
+      }
+      if (records[0].Text__c != null) {
+        mid += "<p>" + records[0].Text__c + "</p>";
+      }
+
+      document.querySelector("#content").innerHTML = start + mid + end;
+    },
+    function (error) {
+      alert("Failed to fetch article: " + error);
     }
   );
 }
